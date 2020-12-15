9b45abe21b0cf6336ee27dd921e43cf3aa86899b
yuusaku125521 <one25Yellow@icloud.com>
2020-12-15 23:59:37 +0900

    u

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import UserIcon from "../icons/user/user";
-import { Nav, NavLink, NavBtn, NavBtnLink, Icon } from "./HeaderElements";
+import { Nav, NavLink, NavBtn, NavBtnLink, Icon, Logo } from "./HeaderElements";
 import Achievement from "./Achievement";
+import rogo from "../../pages/img/rogo.png"
+import { Link } from 'react-router-dom'
+import WhatshotIcon from '@material-ui/icons/Whatshot';
 
 const Header = () => {
   return (
     <>
+      <Logo><Link to="/home"><img src={rogo} alt="logo" style={{ height: "80px", width:"400px"}}/></Link></Logo>
       <Nav>
-        <NavLink to="/">
-          <h1>LeadIT</h1>
-        </NavLink>
+        
         <NavBtn>
           <NavBtnLink to="/ranking">Ranking</NavBtnLink>
         </NavBtn>
+        <WhatshotIcon className={classes.icon} />
         <Achievement />
         <Icon>
           <UserIcon />
--- src/components/templates/Header/HeaderElements.jsx
+++ src/components/templates/Header/HeaderElements.jsx
@@ -1,34 +1,36 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
+import img from "../../pages/img/rogo.png"
+
 
 export const Nav = styled.nav`
-  background: #99ccff;
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1000px) / 2);
-  z-index: 10;
+background:#00DAFF;
+height: 80px;
+display: flex;
+justify-content: space-between;
+padding: 0.5rem calc((100vw - 1000px) / 2);
+z-index: 10;
 `;
 
 export const NavLink = styled(Link)`
-  color: #fff;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
+color: #fff;
+display: flex;
+align-items: center;
+text-decoration: none;
   padding: 0 1rem;
   height: 100%;
   cursor: pointer;
-
+  
   &.active {
     color: black;
   }
-`;
+  `;
 
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
-`;
+  `;
 
 export const NavBtnLink = styled(Link)`
   border-radius: 4px;
@@ -40,18 +42,23 @@ export const NavBtnLink = styled(Link)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-
+  
   &:hover {
     transition: all 0.2s ease-in-out;
     background: #fff;
     color: #010606;
   }
-`;
+  `;
 
 export const Icon = styled.i`
   padding: 10px 22px;
-`;
+  `;
 
 export const Bodyleft = styled.div`
-  float: right;
+float: right;
 `;
+
+export const Logo = styled.div`
+  
+  float: left;
+   `;

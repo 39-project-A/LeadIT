9551a160b30f353884509549bf92daf90f226c7a
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-12-20 23:49:01 +0900

    form-ui-design

--- src/style/formstyle.jsx
+++ src/style/formstyle.jsx
@@ -3,7 +3,7 @@ export const BODY = styled.div`
   font-family: verdana;
 `;
 export const CONTAINER = styled.section`
-  max-width: 50%;
+  max-width: 40%;
   min-width: 30%;
   margin: 0 auto;
   margin-top: 2em;
@@ -34,21 +34,23 @@ export const LI = styled.li`
 `;
 export const LABEL = styled.label`
   display: block;
+  width: 100%;
   font-size: 0.8125em;
   position: absolute;
   top: 1.6em;
-  left: 1.4em;
   color: #f1773b;
   opacity: 1;
+  border-bottom: 1px solid #ff7f50;
 `;
 export const TEXTLABEL = styled.label`
   display: block;
+  width: 100%;
   font-size: 0.8125em;
   position: absolute;
   top: -1.6em;
-  left: 1.4em;
   color: #f1773b;
   opacity: 1;
+  border-bottom: 1px solid #ff7f50;
 `;
 export const SUBMIT = styled.input`
   display: block;
@@ -62,7 +64,7 @@ export const SUBMIT = styled.input`
   height: 100%;
   border: 0;
   outline: none;
-  padding: 2.25em 1em 1em;
+  padding: 1em 1em 1em;
   font-size: 1.2em;
 `;
 export const INPUT = styled.input`
@@ -106,4 +108,21 @@ export const TAGS = styled.div`
   outline: none;
   padding: 2.25em 1em 1em;
   font-size: 1.2em;
+`;
+export const ERR = styled.span`
+  color: red;
+  font-size: 13px;
+`;
+export const ALRADY = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  margin-top: 200px;
+  padding: 2.5em;
+  background: -moz-linear-gradient(#ffb03c, #ff708d);
+  background: -webkit-linear-gradient(#ffb03c, #ff708d);
+  background: linear-gradient(to right, #ffb03c, #ff708d);
+  color: #fff;
+`;
+export const TY = styled.p`
+  font-size: 70px;
 `;

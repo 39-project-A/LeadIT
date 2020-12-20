e492ba67327b9574cdcff87efc11858fa954b387
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-12-20 17:00:28 +0900

    linkfix

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -1,36 +1,61 @@
 import React from "react";
 import UserIcon from "../icons/user/user";
-import { Nav, ItemTop, NavBtn, Icon, LogoItem, HoverText } from "./HeaderElements";
+import {
+  Nav,
+  ItemTop,
+  NavBtn,
+  Icon,
+  LogoItem,
+  HoverText,
+} from "./HeaderElements";
 import Achievement from "./Achievement";
-import rogo from "../../pages/img/logo.png"
-import { Link } from 'react-router-dom'
+import rogo from "../../pages/img/logo.png";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCrown, faClipboard } from "@fortawesome/free-solid-svg-icons";
 
-
 const Header = () => {
   const items = {
-    textDecoration: 'none',
-    color: 'white',
+    textDecoration: "none",
+    color: "white",
     fontSize: "16px",
     padding: "10px",
     textAlin: "center",
     transition: "all 0.2s",
     marginTop: "3px",
-    '&:hover': {
+    "&:hover": {
       background: "#f00",
     },
-  }
+  };
 
   return (
     <>
       <Nav>
-
-        <Link to="/home"><NavBtn ><img src={rogo} alt="logo" style={{ height: "50px", width: "249px", marginTop: "10px" }} /></NavBtn></Link>
+        <Link to="/">
+          <NavBtn>
+            <img
+              src={rogo}
+              alt="logo"
+              style={{ height: "50px", width: "249px", marginTop: "10px" }}
+            />
+          </NavBtn>
+        </Link>
 
         <LogoItem>
-          <Link style={items} to="/ranking">  <HoverText><FontAwesomeIcon icon={faCrown} /><h6 style={{ fontSize: "13px" ,marginTop: "2px"}} >Ranking</h6></HoverText></Link>
-          <Link style={items} to="/ourdots"> <HoverText><FontAwesomeIcon icon={faClipboard} /><h6 style={{ fontSize: "13px",marginTop:"2px" }}>OurDots</h6></HoverText></Link>
+          <Link style={items} to="/ranking">
+            {" "}
+            <HoverText>
+              <FontAwesomeIcon icon={faCrown} />
+              <h6 style={{ fontSize: "13px", marginTop: "2px" }}>Ranking</h6>
+            </HoverText>
+          </Link>
+          <Link style={items} to="/ourdots">
+            {" "}
+            <HoverText>
+              <FontAwesomeIcon icon={faClipboard} />
+              <h6 style={{ fontSize: "13px", marginTop: "2px" }}>OurDots</h6>
+            </HoverText>
+          </Link>
           <ItemTop>
             <Achievement />
           </ItemTop>
@@ -44,4 +69,3 @@ const Header = () => {
 };
 
 export default Header;
-

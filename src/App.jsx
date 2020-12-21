600ab6d7f5881191b3025186b4fed9892fb75b31
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-12-22 00:06:53 +0900

    getDate追加

--- src/App.jsx
+++ src/App.jsx
@@ -17,59 +17,59 @@ import Edit from "./components/pages/Edit";
 import Ranking from "./components/pages/Ranking";
 
 export default function App() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		firebase
-			.firestore()
-			.collection("dots")
-			.get()
-			.then((data) => {
-				const dotData = data.docs.map((doc) => {
-					return {
-						dotId: doc.data().dotId,
-						title: doc.data().title,
-						text: doc.data().text,
-						// url: data.url,
-						working: doc.data().working,
-						tags: doc.data().tags,
-						userId: doc.data().userId,
-						userName: doc.data().userName,
-						// createdAt: doc.data().createdAt,
-						createdAt: new Date(doc.data().createdAt.seconds * 1000), //new
-						getday: doc.data().getday,
-					};
-				});
-				dispatch(fetch_dots(dotData));
-			});
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("dots")
+      .get()
+      .then((data) => {
+        const dotData = data.docs.map((doc) => {
+          return {
+            dotId: doc.data().dotId,
+            title: doc.data().title,
+            text: doc.data().text,
+            working: doc.data().working,
+            tags: doc.data().tags,
+            userId: doc.data().userId,
+            userName: doc.data().userName,
+            // createdAt: doc.data().createdAt,
+            createdAt: new Date(doc.data().createdAt.seconds * 1000), //new
+            getDate: doc.data().getDate,
+            getday: doc.data().getday,
+          };
+        });
+        dispatch(fetch_dots(dotData));
+      });
 
-		firebase
-			.firestore()
-			.collection("userIcon")
-			.get()
-			.then((data) => {
-				const iconsData = data.docs.map((doc) => {
-					return doc.data();
-				});
-				dispatch(fetch_icons(iconsData));
-			});
-	}, []);
+    firebase
+      .firestore()
+      .collection("userIcon")
+      .get()
+      .then((data) => {
+        const iconsData = data.docs.map((doc) => {
+          return doc.data();
+        });
+        dispatch(fetch_icons(iconsData));
+      });
+  }, []);
 
-	return (
-		<AuthProvider>
-			<Router>
-				<Switch>
-					<LoggedInRoute exact path="/" component={Base} />
-					<Route exact path="/home" component={Home} />
-					<Route exact path="/signin" component={SignIn} />
-					<Route exact path="/signup" component={SignUp} />
-					<Route exact path="/form" component={Form} />
-					<Route exact path="/dot/:id" component={DotDetail} />
-					<Route exact path="/dot/:id/edit" component={Edit} />
-					<Route exact path="/ourdots" component={OurDots} />
-					<Route exact path="/ranking" component={Ranking} />
-				</Switch>
-			</Router>
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <LoggedInRoute exact path="/" component={Base} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/form" component={Form} />
+          <Route exact path="/dot/:id" component={DotDetail} />
+          <Route exact path="/dot/:id/edit" component={Edit} />
+          <Route exact path="/ourdots" component={OurDots} />
+          <Route exact path="/ranking" component={Ranking} />
+        </Switch>
+      </Router>
+    </AuthProvider>
+  );
 }

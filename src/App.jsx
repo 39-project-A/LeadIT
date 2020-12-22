1a14ac71c6dfcfb86c4c3c6238d6d601e200c918
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-12-22 14:44:46 +0900

    err修正

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ export default function App() {
               <Route exact path="/signup" component={SignUp} />
               <LoggedInRoute exact path="/form" component={Form} />
               <Route exact path="/dot/:id" component={DotDetail} />
-              <LoggedInRoute exact path="/dot/:id/edit" component={Edit} />
+              <Route exact path="/dot/:id/edit" component={Edit} />
               <LoggedInRoute exact path="/ourdots" component={OurDots} />
               <LoggedInRoute exact path="/ranking" component={Ranking} />
             </Switch>

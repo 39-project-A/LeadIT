416cd4970a79881739d576cb9843b613d6e394d0
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-12-22 21:51:48 +0900

    route修正

--- src/App.jsx
+++ src/App.jsx
@@ -22,119 +22,119 @@ import { complete_loading } from "./reducks/loading/action";
 import { Redirect } from "react-router-dom";
 
 const TITLE = styled.h1`
-	width: 233px;
-	position: relative;
-	top: 43vh;
-	margin: auto;
-	font-family: cursive;
+  width: 233px;
+  position: relative;
+  top: 43vh;
+  margin: auto;
+  font-family: cursive;
 `;
 
 export default function App() {
-	const [loading, set_loading] = useState();
-	const dispatch = useDispatch();
-	const reduxLoading = useSelector((state) => state.loading);
-	function get_userPromise() {
-		return new Promise((resolve, reject) => {
-			setTimeout(() => {
-				try {
-					if (firebase.auth().currentUser === null) {
-						throw new Error("promise error");
-					} else {
-						resolve("complete");
-					}
-				} catch (err) {
-					reject("default");
-				}
-			}, 2000);
-		});
-	}
-	async function get_userData() {
-		const user = await get_userPromise();
-		set_loading(user);
-	}
-	const LoggedInRoute = ({ component: Component }) => {
-		useEffect(() => {
-			set_loading(reduxLoading);
-		});
-		if (reduxLoading === "default") {
-			console.log(loading);
-			return <Route render={(props) => <Redirect to={"/home"} />} />;
-		} else {
-			return <Route render={(props) => <Component {...props} />} />;
-		}
-	};
+  const [loading, set_loading] = useState();
+  const dispatch = useDispatch();
+  const reduxLoading = useSelector((state) => state.loading);
+  function get_userPromise() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          if (firebase.auth().currentUser === null) {
+            throw new Error("promise error");
+          } else {
+            resolve("complete");
+          }
+        } catch (err) {
+          reject("default");
+        }
+      }, 2000);
+    });
+  }
+  async function get_userData() {
+    const user = await get_userPromise();
+    set_loading(user);
+  }
+  const LoggedInRoute = ({ component: Component }) => {
+    useEffect(() => {
+      set_loading(reduxLoading);
+    });
+    if (reduxLoading === "default") {
+      console.log(loading);
+      return <Route render={(props) => <Redirect to={"/home"} />} />;
+    } else {
+      return <Route render={(props) => <Component {...props} />} />;
+    }
+  };
 
-	useEffect(() => {
-		get_userData()
-			.then(() => {
-				dispatch(complete_loading("complete"));
-				set_loading("complete");
-			})
-			.catch(() => {
-				dispatch(complete_loading("default"));
-				set_loading("default");
-			});
-	}, []);
+  useEffect(() => {
+    get_userData()
+      .then(() => {
+        dispatch(complete_loading("complete"));
+        set_loading("complete");
+      })
+      .catch(() => {
+        dispatch(complete_loading("default"));
+        set_loading("default");
+      });
+  }, []);
 
-	useEffect(() => {
-		firebase
-			.firestore()
-			.collection("dots")
-			.get()
-			.then((data) => {
-				const dotData = data.docs.map((doc) => {
-					return {
-						dotId: doc.data().dotId,
-						title: doc.data().title,
-						text: doc.data().text,
-						working: doc.data().working,
-						tags: doc.data().tags,
-						userId: doc.data().userId,
-						userName: doc.data().userName,
-						createdAt: new Date(doc.data().createdAt.seconds * 1000),
-						getDate: doc.data().getDate,
-						getday: doc.data().getday,
-					};
-				});
-				dispatch(fetch_dots(dotData));
-			});
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("dots")
+      .get()
+      .then((data) => {
+        const dotData = data.docs.map((doc) => {
+          return {
+            dotId: doc.data().dotId,
+            title: doc.data().title,
+            text: doc.data().text,
+            working: doc.data().working,
+            tags: doc.data().tags,
+            userId: doc.data().userId,
+            userName: doc.data().userName,
+            createdAt: new Date(doc.data().createdAt.seconds * 1000),
+            getDate: doc.data().getDate,
+            getday: doc.data().getday,
+          };
+        });
+        dispatch(fetch_dots(dotData));
+      });
 
-		firebase
-			.firestore()
-			.collection("userIcon")
-			.get()
-			.then((data) => {
-				const iconsData = data.docs.map((doc) => {
-					return doc.data();
-				});
-				dispatch(fetch_icons(iconsData));
-			});
-	}, []);
+    firebase
+      .firestore()
+      .collection("userIcon")
+      .get()
+      .then((data) => {
+        const iconsData = data.docs.map((doc) => {
+          return doc.data();
+        });
+        dispatch(fetch_icons(iconsData));
+      });
+  }, []);
 
-	return (
-		<>
-			{loading ? (
-				<AuthProvider>
-					<Router>
-						<Switch>
-							<LoggedInRoute exact path="/" component={Base} />
-							<Route exact path="/home" component={Home} />
-							<Route exact path="/signin" component={SignIn} />
-							<Route exact path="/signup" component={SignUp} />
-							<LoggedInRoute exact path="/form" component={Form} />
-							<LoggedInRoute exact path="/dot/:id" component={DotDetail} />
-							<LoggedInRoute exact path="/dot/:id/edit" component={Edit} />
-							<LoggedInRoute exact path="/ourdots" component={OurDots} />
-							<LoggedInRoute exact path="/ranking" component={Ranking} />
-						</Switch>
-					</Router>
-				</AuthProvider>
-			) : (
-				<>
-					<TITLE>Now Loading</TITLE>
-					<CircleLoading />
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {loading ? (
+        <AuthProvider>
+          <Router>
+            <Switch>
+              <LoggedInRoute exact path="/" component={Base} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/signup" component={SignUp} />
+              <LoggedInRoute exact path="/form" component={Form} />
+              <Route exact path="/dot/:id" component={DotDetail} />
+              <Route exact path="/dot/:id/edit" component={Edit} />
+              <LoggedInRoute exact path="/ourdots" component={OurDots} />
+              <LoggedInRoute exact path="/ranking" component={Ranking} />
+            </Switch>
+          </Router>
+        </AuthProvider>
+      ) : (
+        <>
+          <TITLE>Now Loading</TITLE>
+          <CircleLoading />
+        </>
+      )}
+    </>
+  );
 }

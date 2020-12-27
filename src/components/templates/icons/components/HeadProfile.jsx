d8242845ac64dd06bb25ded2dc67ac9ae119d1be
bloodydark <justiceremain48694802@gmail.com>
2020-12-27 12:01:34 +0900

    応急処置

--- src/components/templates/icons/components/HeadProfile.jsx
+++ src/components/templates/icons/components/HeadProfile.jsx
@@ -6,44 +6,57 @@ import firebase from "firebase";
 import { AuthContext } from "../../../../firebase/AuthService";
 
 export default function HeadProfile() {
-	const [imageSrc, setImageSrc] = useState("");
-	const user = useContext(AuthContext);
-	const currentUser = firebase.auth().currentUser;
-	const db = firebase.firestore().collection("userIcon");
-	const iconsData = useSelector((state) => state.icons);
-	const [userIcon, set_userIcon] = useState();
+  const [imageSrc, setImageSrc] = useState("");
+  const user = useContext(AuthContext);
+  const currentUser = firebase.auth().currentUser;
+  const db = firebase.firestore().collection("userIcon");
+  const iconsData = useSelector((state) => state.icons);
+  const [userIcon, set_userIcon] = useState();
 
-  // redux変更完了
-	useEffect(() => {
-		if (iconsData) {
-			const thisUserData = iconsData.find(
-				(iconData) => iconData.userId === currentUser.uid
-			);
-			set_userIcon(thisUserData);
-		}
-	}, [iconsData]);
+  //   // redux変更完了
+  // 	useEffect(() => {
+  // 		if (iconsData) {
+  // 			const thisUserData = iconsData.find(
+  // 				(iconData) => iconData.userId === currentUser.uid
+  // 			);
+  // 			set_userIcon(thisUserData);
+  // 		}
+  // 	}, [iconsData]);
+  useEffect(() => {
+    if (user) {
+      db.where("userId", "==", user.uid)
+        .limit(1)
+        .get()
+        .then((snapshot) => {
+          snapshot.docs.map((doc) => {
+            const item = doc.data();
+            const blob = item.img;
+            set_userIcon(blob);
+          });
+        });
+    }
+  }, [user]);
 
-
-	return (
-		<div className="container3">
-			<button
-				type="button"
+  return (
+    <div className="container3">
+      <button
+        type="button"
         className=" btn-primary_header rounded-circle  opaque profile-pic3"
         // 👇変更の必要アリ
-				disabled={imageSrc}
-			>
-				{!userIcon && (
-					<FontAwesomeIcon icon={faUserAlt} color="white" size="2x" />
-				)}
-				{userIcon && (
-					<img
-						alt="profile"
-						src={userIcon.img}
-						className="rounded-circle"
-						width="100%"
-					/>
-				)}
-			</button>
-		</div>
-	);
+        disabled={imageSrc}
+      >
+        {!userIcon && (
+          <FontAwesomeIcon icon={faUserAlt} color="white" size="2x" />
+        )}
+        {userIcon && (
+          <img
+            alt="profile"
+            src={userIcon}
+            className="rounded-circle"
+            width="100%"
+          />
+        )}
+      </button>
+    </div>
+  );
 }
